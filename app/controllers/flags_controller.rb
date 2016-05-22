6f1fc4e4aca5a9f7a915b909755b90ade8073d5a
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-05-22 18:20:18 +0100

    use a callback

--- app/controllers/flags_controller.rb
+++ app/controllers/flags_controller.rb
@@ -1,11 +1,11 @@
 class FlagsController < ApplicationController
 	before_filter :authenticate_user!, except: [:view, :add_data]
+  before_action :set_flag, :except => [:new, :create, :view]
 
 	def index
 	end
 
 	def show
-		@flag = Flag.find_by_id(params[:id]) || not_found
 		if @flag.creator != current_user.id
 			not_found
 		end
@@ -29,8 +29,8 @@ class FlagsController < ApplicationController
 	end
 
 	def destroy
-		Flag.find_by_id(params["id"]).delete()
-		FlagData.find_all_by_flag_id(params["id"]).each do |d|
+		@flag.delete()
+		FlagData.find_all_by_flag_id(params[:id]).each do |d|
 			d.delete()
 		end
 		redirect_to "/flags"
@@ -49,28 +49,26 @@ class FlagsController < ApplicationController
 	end
 
 	def add_data
-		flag = Flag.find_by_id(params["flag_id"])
-		if !flag
-			render text: "flag doesn't exist"
-			return
-		end
 		data = FlagData.new
-		data.flag_id = flag.id
-		data.key = params["key"]
-		data.object = params["value"]
+		data.flag_id = @flag.id
+		data.key = params[:key]
+		data.object = params[:value]
 		data.save!
 		render text: "success"
 	end
 
 	def edit
-		@flag = Flag.find_by_id(params["id"].to_i)
 	end
 
 	def update
-		flag = Flag.find_by_id(params["id"])
-		flag.summary = params["flag"]["summary"]
-		flag.title = params["flag"]["title"]
-		flag.save!
-		redirect_to flag_path(params["id"])
+		@flag.summary = params["flag"]["summary"]
+		@flag.title = params["flag"]["title"]
+		@flag.save!
+		redirect_to flag_path(@flag.id)
 	end
+
+  private
+    def set_flag
+      @flag = Flag.find params[:flag_id]
+    end
 end
